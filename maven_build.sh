449db55404f3f1823954e8641fdff1cc405c72f5
srijitcn <srijit.nair@databricks.com>
2023-03-16 13:15:22 -0400

    adding a maven cordinate

--- maven_build.sh
+++ maven_build.sh
@@ -1,1 +1,8 @@
+#To import:
+#repo: https://github.com/databricks-industry-solutions/hls-payer-mrf-sparkstreaming/raw/maven
+#coordinates: databricks-industry-solutions:hls-payer-mrf-sparkstreaming:0.3.5
+#
+#to run install maven, build the jar using sbt package, and use ./maven_build.sh
+
+
 mvn install:install-file -DgroupId=databricks-industry-solutions -DartifactId=hls-payer-mrf-sparkstreaming -Dversion=0.3.5 -Dfile=target/scala-2.12/payer-mrf-streamsource-0.3.4.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
