d4ae5a7f41a58df507e99d8e21dc1e2b7318a127
srijitcn <srijit.nair@databricks.com>
2023-03-16 12:02:49 -0400

    adding a maven cordinate

---
+++ maven_build.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=databricks -DartifactId=payer_mrf_streamsource -Dversion=0.3.5 -Dfile=target/scala-2.12/payer-mrf-streamsource-0.3.4.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
